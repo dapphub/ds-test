b13761facdedb1f99846dbafff44c39b2eb9353d
rain <rainbreak@riseup.net>
2017-10-02 23:39:24 +0100

    suppress setUp pure restriction warning

--- src/test.sol
+++ src/test.sol
@@ -26,12 +26,14 @@ contract DSTest {
 
     bool public IS_TEST;
     bool public failed;
+    bool SUPPRESS_SETUP_WARNING;  // hack for solc pure restriction warning
 
     function DSTest() internal {
         IS_TEST = true;
     }
 
     function setUp() public {
+        SUPPRESS_SETUP_WARNING = true;  // totally unused by anything
     }
 
     function fail() internal {
