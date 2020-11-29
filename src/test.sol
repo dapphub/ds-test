f09dd1d8b4d097ea52ec66515643712d7dd4b4a3
rain <rainbreak@riseup.net>
2020-11-29 13:33:19 +0000

    hevm specific features

--- src/test.sol
+++ src/test.sol
@@ -38,6 +38,12 @@ contract DSTest {
     bool public IS_TEST = true;
     bool public failed;
 
+    address constant HEVM_ADDRESS =
+        address(bytes20(uint160(uint256(keccak256('hevm cheat code')))));
+
+    modifier mayRevert() { _; }
+    modifier testopts(string memory) { _; }
+
     function fail() internal {
         failed = true;
     }
