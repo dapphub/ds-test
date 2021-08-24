fc108509eba838e0b9e52868cef9322047c21c50
Crisgarner <ceegarner@gmail.com>
2021-08-24 10:42:30 -0600

    changed to interface

--- src/hevm.sol
+++ src/hevm.sol
@@ -15,12 +15,12 @@
 
 pragma solidity >=0.4.23;
 
-abstract contract Hevm {
-   function warp(uint256 x) external virtual;
-   function roll(uint256 x) external virtual;
-   function store(address c,bytes32 loc,bytes32 val) external virtual;
-   function load(address c,bytes32 loc) external virtual returns (bytes32 val);
-   function sign(uint256 sk,bytes32 digest) external virtual returns (uint8 v, bytes32 r, bytes32 s);
-   function addr(uint256 sk) external virtual returns (address addr);
-   function ffi(string[] calldata) external virtual returns (bytes memory);
+interface Hevm {
+   function warp(uint256 x) external;
+   function roll(uint256 x) external;
+   function store(address c,bytes32 loc,bytes32 val) external;
+   function load(address c,bytes32 loc) external returns (bytes32 val);
+   function sign(uint256 sk,bytes32 digest) external returns (uint8 v, bytes32 r, bytes32 s);
+   function addr(uint256 sk) external returns (address addr);
+   function ffi(string[] calldata) external returns (bytes memory);
 }
